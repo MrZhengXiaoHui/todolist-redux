99efa5e60010bfb43c41227a70362057b3ae7107
郑小辉 <zhengxiaohui@zhengxiaohuideMacBook-Pro.local>
2019-10-03 18:21:33 +0800

    todolist

---
+++ src/TodoList.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd';
+import store from './store';
+import {
+    getInputChangeAction,
+    getAddItemAction,
+    getDeleteItemAction
+} from './store/actionCreators';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+        store.subscribe(this.handleStoreChange);
+    }
+    render() {
+        return (
+            <div style={{ marginTop: '10px', marginLeft: '10px' }}>
+                <div>
+                    <Input
+                        value={this.state.inputValue}
+                        placeholder="todo info"
+                        style={{ width: '300px' }}
+                        onChange={this.handleInputChange}
+                    />
+                    <Button type="primary" onClick={this.handleBtnClick}>
+                        提交
+                    </Button>
+                </div>
+                <List
+                    style={{ marginTop: '10px', width: '300px' }}
+                    bordered
+                    dataSource={this.state.list}
+                    renderItem={(item, index) => (
+                        <List.Item onClick={() => this.handleItemDelete(index)}>
+                            {item}
+                        </List.Item>
+                    )}
+                />
+            </div>
+        );
+    }
+    handleInputChange = e => {
+        const action = getInputChangeAction(e.target.value);
+        store.dispatch(action);
+    };
+    handleStoreChange = () => {
+        this.setState(store.getState());
+    };
+    handleBtnClick = () => {
+        const action = getAddItemAction();
+        store.dispatch(action);
+    };
+    handleItemDelete = index => {
+        const action = getDeleteItemAction(index);
+        store.dispatch(action);
+    };
+}
+export default TodoList;
